dc7753929dc4f46ccea7a54dff5cbfee31c16804
DELL <DELL@QTUOHSD>
2025-02-24 20:54:37 +0700

    Add project files.

---
+++ models/receipt.py
@@ -0,0 +1,20 @@
+﻿class Receipt:
+    def __init__(self, receipt_id=None, user_id=None, book_id=None, borrow_date=None, return_date=None, status="Borrowed"):
+        self.receipt_id = receipt_id
+        self.user_id = user_id
+        self.book_id = book_id
+        self.borrow_date = borrow_date
+        self.return_date = return_date
+        self.status = status
+        # self.db = Database()  # Kết nối database khi cần
+
+    def save(self):
+        pass
+
+    def get_receipt_by_id(receipt_id):
+        pass
+
+    def return_book():
+        pass
+
+
